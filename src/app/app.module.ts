d69cf0cd46853f11ef99f368f9256071b4d8dc41
DeathScript <robochoa@est.ecotec.edu.ec>
2018-11-30 01:34:16 -0500

    [Update]: App.module
    Addiing of routing to change views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ConversationComponent } from './conversation/conversation.component';
 import { ProfileComponent } from './profile/profile.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'conversation', component:ConversationComponent},
+  {path: 'profile', component:ProfileComponent},
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +27,8 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
