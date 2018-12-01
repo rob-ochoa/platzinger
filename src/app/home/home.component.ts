0fda48b5900185eadb18272b8fcf06bb4ab66d0b
DeathScript <robochoa@est.ecotec.edu.ec>
2018-12-01 00:14:05 -0500

    [Add]: Users.ts
    Se agregaron usuarios para mostrar funciones de typescript y se modifico let for en home.component.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  friends: User[];
+
+  constructor() { 
+    let usuario1 : User = {
+      nick : 'Roberth',
+      age: 20,
+      friend: true,
+      email: 'robochoa@est.ecotec.edu.ec',
+      uid : 1, 
+    };
+    let usuario2 : User = {
+      nick : 'Diana',
+      age: 18,
+      friend: true,
+      email: 'diana@est.ecotec.edu.ec',
+      uid : 1, 
+    };
+    let usuario3 : User = {
+      nick : 'Karen',
+      age: 27,
+      friend: false,
+      email: 'karen@est.ecotec.edu.ec',
+      uid : 1, 
+    };
+    let usuario4 : User = {
+      nick : 'Doris',
+      age: 56,
+      friend: true,
+      email: 'doris@est.ecotec.edu.ec',
+      uid : 1, 
+    };
+    let usuario5 : User = {
+      nick : 'Mireya',
+      age: 57,
+      friend: false,
+      email: 'mireya@est.ecotec.edu.ec',
+      uid : 1, 
+    };
+    this.friends = [usuario1, usuario2, usuario3, usuario4, usuario5];
+  };
+
+
 
   ngOnInit() {
   }
